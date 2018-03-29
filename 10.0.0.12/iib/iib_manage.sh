faef7f9197b8d0b7ac2905d6621f90ade33dbe47
Ashley Freeth <ASHFREET@uk.ibm.com>
2018-03-29 17:04:18 +0100

    Move changes for 10.0.0.12 to new directory

--- 10.0.0.12/iib/iib_manage.sh
+++ 10.0.0.12/iib/iib_manage.sh
@@ -21,7 +21,7 @@ stop()
 start()
 {
 	echo "----------------------------------------"
-        /opt/ibm/iib-10.0.0.11/iib version
+        /opt/ibm/iib-10.0.0.12/iib version
 	echo "----------------------------------------"
 
         NODE_EXISTS=`mqsilist | grep $NODE_NAME > /dev/null ; echo $?`
